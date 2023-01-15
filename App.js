a86d749001c5d9f7eadac47a08b6cd0110d47020
Heli-DL <heli.delorme@outlook.com>
2023-01-15 22:06:21 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [result, setResult] = useState(0);
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  const sumNumbers = () => {  setResult(number1 + number2);}
+
+  const substractNumbers = () => {  setResult(number1 - number2);}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text>Result: {result}</Text>
+        <TextInput style={{width:200, borderColor: 'gray', borderWidth:1}}onChangeText={number1 => setNumber1(Number(number1))}  value={number1} keyboardType={'numeric'}/>
+        <TextInput style={{width:200, borderColor: 'gray', borderWidth:1}}onChangeText={number2 => setNumber2(Number(number2))}  value={number2} keyboardType={'numeric'}/>
+      <View style={styles.buttons}> 
+        <View style={styles.button}>
+          <Button onPress={sumNumbers}title="+" />
+        </View>
+        <View style={styles.button}>
+          <Button onPress={substractNumbers}title="-" />
+        </View>
+      </View>
+      <StatusBar style="auto" /> 
     </View>
   );
 }
@@ -17,4 +36,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttons: {
+    flexDirection: 'row',
+    margin: 10,
+  },
+  button: {
+    width: 30,
+    margin: 10
+  }
 });
